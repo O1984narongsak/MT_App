e7993d16aa9dbdb002ca5505cc7f54f8e4cd49c5
Narongsak Launwijit <censpi@gmail.com>
2018-10-04 15:37:42 +0700

    Initial Commit

---
+++ MT_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MT_App
+//
+//  Created by Narongsak_O on 4/10/18.
+//  Copyright © 2018 nProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
